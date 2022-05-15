524f556b47d2bd2280e7be657ea12773d07b5a52
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-15 16:05:56 -0400

    draw square

---
+++ notes.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("OliveDrab")
+timmy_the_turtle.color("OliveDrab")
+
+def draw_square():
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+draw_square()
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
