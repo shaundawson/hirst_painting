43951773e85130c8cd6fe70df52f9f7281d3bc95
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-15 16:08:32 -0400

    draw square with for loop

--- notes.py
+++ notes.py
@@ -5,20 +5,22 @@ timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("OliveDrab")
 timmy_the_turtle.color("OliveDrab")
 
-def draw_square():
+# def draw_square():
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+# draw_square()
+
+
+for _ in range(4):
     timmy_the_turtle.forward(100)
     timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
-
-draw_square()
-
-
-
 
 
 
