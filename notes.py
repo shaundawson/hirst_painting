89ff236e798bd939c2c2050ccfc1c88d68a868bb
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-15 18:15:53 -0400

    add colors

--- notes.py
+++ notes.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
@@ -47,6 +48,9 @@ timmy_the_turtle.color("OliveDrab")
 #     timmy_the_turtle.forward(l)
 #     timmy_the_turtle.right(360 / n)
 
+colours = ["DarkOrchid", "IndianRed", "wheat", "SeaGreen"]
+
+
 def draw_shape(num_sides):
     angle = 360 / num_sides
     for _ in range(num_sides):
@@ -54,6 +58,7 @@ def draw_shape(num_sides):
         timmy_the_turtle.right(angle)
 
 for shape_side_n in range(3,11):
+    timmy_the_turtle.color(random.choice(colours))
     draw_shape(shape_side_n)
 
 
