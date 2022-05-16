c54876fdcf0325d704b933e0541220aed445c67b
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 12:50:22 -0400

    change speed of animation

--- notes.py
+++ notes.py
@@ -63,6 +63,7 @@ colours = ["CornflowerBlue","DarkOrchid", "IndianRed", "wheat","DeepSkyBlue", "L
 
 directions = [0, 90, 180, 270]
 timmy_the_turtle.pensize(15)
+timmy_the_turtle.speed("fastest")
 
 for _ in range(200):
     timmy_the_turtle.color(random.choice(colours))
