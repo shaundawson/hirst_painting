444f5d164820f0f409954aa48c323aa35b45fa63
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 13:33:00 -0400

    random color with rgb tuple

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hirst_painting
  Python #100DaysofCode: Turtle Graphics, Tuples and Importing Modules
+
+Turtle Graphics Documentation:
+https://docs.python.org/3/library/turtle.html
+
+RGB Colors Tool
+https://www.w3schools.com/colors/colors_rgb.asp
--- notes.py
+++ notes.py
@@ -1,36 +1,46 @@
-from turtle import Turtle, Screen
+import turtle as t
+from turtle import Screen
 import random
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("OliveDrab")
-timmy_the_turtle.color("OliveDrab")
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r,g,b)
+    return random_color
+
+tim.shape("turtle")
+tim.color("OliveDrab")
+tim.color("OliveDrab")
 
 # def draw_square():
-#     timmy_the_turtle.forward(100)
-#     timmy_the_turtle.right(90)
-#     timmy_the_turtle.forward(100)
-#     timmy_the_turtle.right(90)
-#     timmy_the_turtle.forward(100)
-#     timmy_the_turtle.right(90)
-#     timmy_the_turtle.forward(100)
-#     timmy_the_turtle.right(90)
+#     tim.forward(100)
+#     tim.right(90)
+#     tim.forward(100)
+#     tim.right(90)
+#     tim.forward(100)
+#     tim.right(90)
+#     tim.forward(100)
+#     tim.right(90)
 
 # draw_square()
 
 # for _ in range(4):
-#     timmy_the_turtle.forward(100)
-#     timmy_the_turtle.right(90)
+#     tim.forward(100)
+#     tim.right(90)
 
 
 
 
 #Draw Dashed Line
 # for i in range(15):
-#     timmy_the_turtle.forward(10)
-#     timmy_the_turtle.penup()
-#     timmy_the_turtle.forward(10)
-#     timmy_the_turtle.pendown()
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
 
 
@@ -45,30 +55,29 @@ timmy_the_turtle.color("OliveDrab")
   
 
 # for _ in range(n):
-#     timmy_the_turtle.forward(l)
-#     timmy_the_turtle.right(360 / n)
+#     tim.forward(l)
+#     tim.right(360 / n)
 
-colours = ["CornflowerBlue","DarkOrchid", "IndianRed", "wheat","DeepSkyBlue", "LightSeaGreen", "SlateGray", "SeaGreen"]
-
+# colours = ["CornflowerBlue","DarkOrchid", "IndianRed", "wheat","DeepSkyBlue", "LightSeaGreen", "SlateGray", "SeaGreen"]
 
 # def draw_shape(num_sides):
 #     angle = 360 / num_sides
 #     for _ in range(num_sides):
-#         timmy_the_turtle.forward(100)
-#         timmy_the_turtle.right(angle)
+#         tim.forward(100)
+#         tim.right(angle)
 
 # for shape_side_n in range(3,11):
-#     timmy_the_turtle.color(random.choice(colours))
+#     tim.color(random.choice(colours))
 #     draw_shape(shape_side_n)
 
 directions = [0, 90, 180, 270]
-timmy_the_turtle.pensize(15)
-timmy_the_turtle.speed("fastest")
+tim.pensize(15)
+tim.speed("fastest")
 
 for _ in range(200):
-    timmy_the_turtle.color(random.choice(colours))
-    timmy_the_turtle.forward(30)
-    timmy_the_turtle.setheading(random.choice(directions))
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 
 screen = Screen()
