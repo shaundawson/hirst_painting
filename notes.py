c62743b58e760be9da50b73debfbfe9b8d3485b6
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 13:51:09 -0400

    draw spirograph

--- notes.py
+++ notes.py
@@ -1,16 +1,25 @@
 import turtle as t
-from turtle import Screen
 import random
 
 tim = t.Turtle()
 t.colormode(255)
+tim.speed("fastest")
 
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-    random_color = (r,g,b)
-    return random_color
+    rand_color = (r,g,b)
+    return rand_color
+
+#Make a Spirograph
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+draw_spirograph(5)
+
 
 tim.shape("turtle")
 tim.color("OliveDrab")
@@ -70,15 +79,14 @@ tim.color("OliveDrab")
 #     tim.color(random.choice(colours))
 #     draw_shape(shape_side_n)
 
-directions = [0, 90, 180, 270]
-tim.pensize(15)
-tim.speed("fastest")
+# directions = [0, 90, 180, 270]
+# tim.pensize(10)
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
 
 
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
